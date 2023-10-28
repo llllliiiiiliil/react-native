acd2870145e350a64c269f5ce02ca153434cf102
박한 <bagkan380@gmail.com>
2023-10-28 12:56:45 +0900

    asd

--- src/components/footer/style.ts
+++ src/components/footer/style.ts
@@ -8,3 +8,20 @@ export const FooterBox = styled.View`
   bottom: 0;
   border-top: 10px solid red;
 `;
+
+export const EmogeBox = styled.View`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+export const Texts = styled.Text`
+  font-size: 13px;
+  font-weight: 900;
+`;
+
+export const Emoge = styled.Text`
+  font-size: 22px;
+`;
--- src/components/footer/view/index.tsx
+++ src/components/footer/view/index.tsx
@@ -2,5 +2,16 @@ import React from "react";
 import * as S from "../style";
 
 export const FooterView = () => {
-  return <S.FooterBox></S.FooterBox>;
+  return (
+    <S.FooterBox style={{ borderTopWidth: 1, borderTopColor: "gainsboro" }}>
+      <S.EmogeBox>
+        <S.Emoge>🏠</S.Emoge>
+        <S.Texts>홈</S.Texts>
+        <S.Emoge>⏰</S.Emoge>
+        <S.Texts>시간표</S.Texts>
+        <S.Emoge>🔍</S.Emoge>
+        <S.Texts>더보기</S.Texts>
+      </S.EmogeBox>
+    </S.FooterBox>
+  );
 };
